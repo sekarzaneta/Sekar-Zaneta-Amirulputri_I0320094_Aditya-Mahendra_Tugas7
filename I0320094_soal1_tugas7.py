31c87114400686ea1e2bba3e071021cccdd31e66
Sekar Zaneta <masayusekar@gmail.com>
2021-04-16 23:20:20 +0700

    tugas

--- I0320094_soal1_tugas7.py
+++ I0320094_soal1_tugas7.py
@@ -1,1 +1,19 @@
+print("==== Penggunaan String ====")
 
+identitas = input("Masukkan nama panggilan Anda: ")
+str = "good night,"
+s = str.capitalize()
+nama = identitas.capitalize()
+print(s, nama)
+
+str = "good night,"
+waktu = str.replace("night", "morning")
+s = waktu.capitalize()
+print(s, nama)
+
+a = nama.count("a")
+i = nama.count ("i")
+u = nama.count("u")
+e = nama.count ("e")
+o = nama.count("o")
+print("Huruf vokal yang terdapat pada nama Anda adalah" ,a + i + e + o, "huruf")
--- I0320094_soal2_tugas7.py
+++ I0320094_soal2_tugas7.py
@@ -1,1 +1,16 @@
+print("==== Penggunaan Numerik ====")
 
+nilai = input("Masukkan nilai siswa: ").split()
+
+for i in range(0,len(nilai)):
+    nilai[i] = int(nilai[i])
+
+jumlah = len(nilai)
+ratarata = sum(nilai)/jumlah
+
+import math
+print("Nilai valid rata-rata seluruh siswa: ", math.ceil(ratarata))
+
+print("Nilai siswa tertinggi adalah ", max(nilai))
+
+print("Nilai siswa terendah adalah ", min(nilai))
